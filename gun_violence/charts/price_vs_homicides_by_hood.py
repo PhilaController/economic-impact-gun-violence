36fb8bb5cc30bcb4ab14e392fefa5df2d476643f
Nick Hand <nicholas.adam.hand@gmail.com>
2019-08-23 10:15:17 -0400

    split into two figures

--- gun_violence/charts/price_vs_homicides_by_hood.py
+++ gun_violence/charts/price_vs_homicides_by_hood.py
@@ -9,7 +9,10 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
+import matplotlib.transforms as transforms
 from phila_colors import palette
+from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, HPacker
+import os
 
 YEAR_LIMIT = 2018
 
@@ -18,6 +21,8 @@ def _format_neighborhood_name(x):
     """
     Internal function to format neighborhood names nicely.
     """
+    if x == "Aston-Woodbridge":
+        return "Aston\nWoodbridge"
     x = x.replace("-", "")
     fields = x.split()
     if x == "East Oak Lane":
@@ -45,7 +50,6 @@ def _plot_sales(ax, df, col, **kws):
     """
 
     ax.plot(df["sale_year"], df[col], **kws)
-    sns.despine(left=True)
     ax.tick_params(labelleft=False, left=False, labelbottom=False, bottom=False)
 
 
@@ -62,7 +66,6 @@ def _plot_homicides(ax, df, **kws):
         clip_on=False,
         **kws,
     )
-    sns.despine(left=True, right=True)
     ax.tick_params(
         labelleft=False,
         left=False,
@@ -114,177 +117,237 @@ def plot(fig_num, outfile):
         .tolist()
     )
 
+    # trim to first 126 panels
+    num_panels = 126
+    neighborhoods = neighborhoods[:num_panels]
+
     with plt.style.context("fivethirtyeight"):
         plt.rcParams.update(default_style)
         plt.rcParams["axes.edgecolor"] = "black"
         plt.rcParams["patch.edgecolor"] = "black"
         plt.rcParams["axes.linewidth"] = 1.0
 
-        # Create the figure
-        nrows = 14
-        ncols = 9
-        fig, axs = plt.subplots(
-            nrows=nrows,
-            ncols=ncols,
-            figsize=(6.45, 10),
-            gridspec_kw=dict(
-                left=0.025, right=0.95, bottom=0.045, top=0.88, hspace=0.9, wspace=0.3
-            ),
-        )
-
         # Trim to top neighborhoods
         df = median_sale_price.query("neighborhood in @neighborhoods")
 
         # Plot each panel
-        panel = 0
-        for i in range(nrows):
-            for j in range(ncols):
 
-                # The axes for this panel
-                ax = axs[i, j]  # sale price
-                ax2 = ax.twinx()  # homicides
+        for subplot in [0, 1]:
 
-                # Blank panel
-                if panel >= len(neighborhoods):
-                    ax.set_axis_off()
-                    continue
+            figure_tag = "A" if subplot == 0 else "B"
 
-                # The neighborhood we are plotting
-                hood = neighborhoods[panel]
+            # first figure or second
+            if subplot == 0:
+                panel = 0
+            else:
+                panel = num_panels // 2
 
-                # Plot the homicides
-                _plot_homicides(
-                    ax2,
-                    homicide_count.query("neighborhood == @hood"),
-                    color=palette["love-park-red"],
-                )
+            # Create the figure
+            nrows = 9
+            ncols = 7
+            TOP = 0.85
+            fig, axs = plt.subplots(
+                nrows=nrows,
+                ncols=ncols,
+                figsize=(6.45, 10),
+                gridspec_kw=dict(
+                    left=0.025, right=0.95, bottom=0.06, top=TOP, hspace=0.9, wspace=0.3
+                ),
+            )
 
-                # Plot the sales
-                _plot_sales(
-                    ax,
-                    median_sale_price.query("neighborhood == @hood"),
-                    "sale_price",
-                    linewidth=2,
-                    clip_on=False,
-                    zorder=19,
-                    color=palette["ben-franklin-blue"],
-                )
+            for i in range(nrows):
+                for j in range(ncols):
 
-                # Add a legend
-                if i == 0 and j == ncols - 1:
-                    legend_elements = [
-                        Line2D(
-                            [0],
-                            [0],
-                            color="#2176d2",
-                            lw=2,
-                            label="Median Residential Sale Price",
-                        ),
-                        Patch(facecolor="#cc3000", label="Number of Homicides"),
-                    ]
-                    ax.legend(
-                        handles=legend_elements,
-                        loc="lower center",
-                        bbox_transform=fig.transFigure,
-                        bbox_to_anchor=(0.5, 0.9),
-                        ncol=2,
-                        fontsize=10,
-                        frameon=False,
+                    # The axes for this panel
+                    ax = axs[i, j]  # sale price
+                    ax2 = ax.twinx()  # homicides
+
+                    # Blank panel
+                    if panel >= len(neighborhoods):
+                        ax.set_axis_off()
+                        continue
+
+                    # The neighborhood we are plotting
+                    hood = neighborhoods[panel]
+
+                    # Plot the homicides
+                    _plot_homicides(
+                        ax2,
+                        homicide_count.query("neighborhood == @hood"),
+                        color=palette["love-park-red"],
                     )
 
-                # x ticks
-                ax.set_xticks(range(2006, 2019, 3))
-                ax2.set_xticks(range(2006, 2019, 3))
-
-                # Put original axes on top
-                ax.set_zorder(ax2.get_zorder() + 1)
-                ax.patch.set_visible(False)
-
-                # Grid for original axes
-                for xtick in ax2.get_xticks():
-                    ax2.axvline(
-                        x=xtick, color="#a1a1a1", lw=0.5, ls="dashed", zorder=10
+                    # Plot the sales
+                    # This is the border
+                    _plot_sales(
+                        ax,
+                        median_sale_price.query("neighborhood == @hood"),
+                        "sale_price",
+                        linewidth=3.5,
+                        clip_on=False,
+                        zorder=19,
+                        color=palette["sidewalk"],
+                    )
+                    _plot_sales(
+                        ax,
+                        median_sale_price.query("neighborhood == @hood"),
+                        "sale_price",
+                        linewidth=2.5,
+                        clip_on=False,
+                        zorder=20,
+                        color=palette["ben-franklin-blue"],
                     )
 
-                # Subplot title
-                ax.text(
-                    0.5,
-                    1.1,
-                    _format_neighborhood_name(hood),
-                    fontsize=6,
-                    transform=ax.transAxes,
-                    ha="center",
-                    va="bottom",
-                    bbox=dict(facecolor="white", edgecolor="none", pad=0),
-                )
+                    # Add a legend
+                    if i == 0 and j == ncols - 1:
+                        legend_elements = [
+                            Line2D(
+                                [0],
+                                [0],
+                                color="#2176d2",
+                                lw=2,
+                                label="Median Residential Sale Price",
+                            ),
+                            Patch(facecolor="#cc3000", label="Number of Homicides"),
+                        ]
+                        ax.legend(
+                            handles=legend_elements,
+                            loc="lower center",
+                            bbox_transform=fig.transFigure,
+                            bbox_to_anchor=(0.5, TOP + 0.03),
+                            ncol=2,
+                            fontsize=10,
+                            frameon=False,
+                        )
 
-                # Format axes
-                ax.set_ylim(bottom=1e3)
-                ax.grid(b=False, axis="both")
+                    # x ticks
+                    ax.set_xticks(range(2006, 2019, 3))
+                    ax2.set_xticks(range(2006, 2019, 3))
 
-                ax2.set_ylim(0, 20)
-                ax2.grid(b=False, axis="both")
+                    # Put original axes on top
+                    ax.set_zorder(ax2.get_zorder() + 1)
+                    ax.patch.set_visible(False)
 
-                # More formatting
-                if i == nrows - 1:
-                    ax.tick_params(labelbottom=True)
-                    plt.setp(ax.get_xticklabels(), rotation=90, fontsize=7)
-                if j == ncols - 1:
+                    # Grid for original axes
+                    for xtick in ax2.get_xticks():
+                        ax2.axvline(
+                            x=xtick, color="#a1a1a1", lw=0.5, ls="dashed", zorder=10
+                        )
+
+                    # Subplot title
+                    ax.text(
+                        0.5,
+                        1.1,
+                        _format_neighborhood_name(hood),
+                        fontsize=9,
+                        transform=ax.transAxes,
+                        ha="center",
+                        va="bottom",
+                        bbox=dict(facecolor="white", edgecolor="none", pad=0),
+                    )
+
+                    # Format axes
+                    ax.set_ylim(bottom=1e3)
+                    ax.grid(b=False, axis="both")
+
+                    ax2.set_ylim(0, 20)
+                    ax2.grid(b=False, axis="both")
+
+                    # More formatting
+                    if i == nrows - 1:
+                        ax.tick_params(labelbottom=True)
+                        plt.setp(ax.get_xticklabels(), rotation=90, fontsize=9)
+
+                    # homicide ticks
                     ax2.set_yticks([0, 5, 10, 15, 20])
-                    ax2.tick_params(labelright=True, right=True, width=1, length=3)
-                    plt.setp(ax2.get_yticklabels(), fontsize=7)
+                    ax2.tick_params(labelright=False, right=True, width=1, length=3)
+                    if j == ncols - 1:
+                        ax2.tick_params(labelright=True)
+                        plt.setp(ax2.get_yticklabels(), fontsize=9)
 
-                panel += 1
+                    # if j == ncols - 1:
+                    for a in [ax, ax2]:
+                        for spine in ["left", "top"]:
+                            a.spines[spine].set_visible(False)
 
-        # Add title
-        fig.text(
-            0.005,
-            0.99,
-            f"Figure {fig_num}",
-            weight="bold",
-            fontsize=10,
-            ha="left",
-            va="top",
-        )
-        fig.text(
-            0.005,
-            0.975,
-            "Trends in Residential Sale Prices & Total Homicides in Philadelphia's Neighborhoods",
-            weight="bold",
-            fontsize=11,
-            ha="left",
-            va="top",
-        )
-        fig.text(
-            0.005,
-            0.955,
-            "Neighborhoods are sorted by median sale price in 2018, starting with the most expensive (top left)",
-            fontsize=10,
-            ha="left",
-            va="top",
-            style="italic",
-        )
-        fig.text(
-            0.9575,
-            0.895,
-            "Total\nHomicides",
-            weight="bold",
-            ha="left",
-            rotation=90,
-            fontsize=8,
-        )
+                    panel += 1
 
-        # Add the footnote
-        footnote = r"$\bf{Sources}$: Office of Property Assessment, Police Department"
-        fig.text(
-            0.005,
-            0.002,
-            footnote,
-            fontsize=6,
-            color=palette["dark-gray"],
-            ha="left",
-            va="bottom",
-        )
+            # Add title
+            fig.text(
+                0.005,
+                0.99,
+                f"Figure {fig_num}{figure_tag}",
+                weight="bold",
+                fontsize=10,
+                ha="left",
+                va="top",
+            )
+            # First line of title
+            fig.text(
+                0.005,
+                0.975,
+                "Trends in Residential Sale Prices & Total Homicides for Neighborhoods",
+                weight="bold",
+                fontsize=11,
+                ha="left",
+                va="top",
+            )
 
-        # Save!
-        plt.savefig(outfile, dpi=300)
+            tag = "Above" if subplot == 0 else "Below"
+
+            # Build multi-color title
+            textprops = dict(color="k", weight="bold", size=11, ha="left", va="top")
+            t1 = TextArea("\nWith", textprops=textprops)
+            t2 = TextArea(
+                f"\nSale Prices {tag} the Citywide Median",
+                textprops={**textprops, "color": palette["love-park-red"]},
+            )
+            t3 = TextArea("\nin 2018", textprops=textprops)
+            titleBox = HPacker(children=[t1, t2, t3], align="left", pad=0, sep=3)
+            subtitle = AnchoredOffsetbox(
+                loc="lower left",
+                child=titleBox,
+                pad=0.0,
+                frameon=False,
+                bbox_to_anchor=(0.005, 0.97),
+                bbox_transform=fig.transFigure,
+                borderpad=0.0,
+            )
+            fig.add_artist(subtitle)
+
+            fig.text(
+                0.005,
+                0.937,
+                "Neighborhoods sorted by median sale price in 2018, from most (top left) to least (bottom right) expensive",
+                fontsize=10,
+                ha="left",
+                va="top",
+                style="italic",
+            )
+            fig.text(
+                0.9575,
+                TOP + 0.01,
+                "Total\nHomicides",
+                weight="bold",
+                ha="left",
+                rotation=90,
+                fontsize=8,
+            )
+
+            # Add the footnote
+            footnote = (
+                r"$\bf{Sources}$: Office of Property Assessment, Police Department"
+            )
+            fig.text(
+                0.005,
+                0.002,
+                footnote,
+                fontsize=6,
+                color=palette["dark-gray"],
+                ha="left",
+                va="bottom",
+            )
+
+            # Save!
+            path, ext = os.path.splitext(outfile)
+            plt.savefig(f"{path}_{subplot}{ext}", dpi=300)
