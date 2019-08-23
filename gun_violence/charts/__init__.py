9610a0813b27edda2819780e183859465078d05e
Nick Hand <nicholas.adam.hand@gmail.com>
2019-08-23 10:15:26 -0400

    import cost benefit figure

--- gun_violence/charts/__init__.py
+++ gun_violence/charts/__init__.py
@@ -15,3 +15,4 @@ from .homicides_by_hood import plot as homicides_by_hood
 from .prices_near_homicides import plot as prices_near_homicides
 from .population_change import plot as population_change
 from .homicides_poverty import plot as homicides_poverty
+from .cost_benefit import plot as cost_benefit
