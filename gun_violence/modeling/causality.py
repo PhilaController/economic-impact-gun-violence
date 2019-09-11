f573f55a7be31938c1415dc2a2fdea9f5a948a20
Nick Hand <nicholas.adam.hand@gmail.com>
2019-09-11 12:35:33 -0400

    update the calculation of flags

--- gun_violence/modeling/causality.py
+++ gun_violence/modeling/causality.py
@@ -94,14 +94,16 @@ def _get_sale_groups_by_homicide(homicides, sales, space_radius, time_window):
         yield i, dt, indices_before_after, dists_before_after
 
 
-def add_spacetime_flags(homicides, sales, distances, windows, window_sales=True):
+def add_spacetime_flags(
+    homicides, sales, distances, windows, window_sales=True, add_interactions=True
+):
     """
     Add flags to the input sales if a homicide occurs within a given time window 
     and a given spatial radius from the sale. 
 
     Notes
     -----
-    The added columns are "spacetime_before_X.X" and "spacetime_after_X.X".
+    The added columns are "spacetime_flag_X.X" and "spacetime_flag_X.X".
 
     Parameters
     ----------
@@ -116,6 +118,9 @@ def add_spacetime_flags(homicides, sales, distances, windows, window_sales=True)
         giving the window before and after the sale date to search
     window_sales : bool, optional
         whether to remove invalid sales based on the time window given
+    add_interactions : bool, optional
+        if `True`, add the flags that are the difference of the post/pre flags
+        for each distance 
 
     Returns
     -------
@@ -124,13 +129,19 @@ def add_spacetime_flags(homicides, sales, distances, windows, window_sales=True)
     """
     if not isinstance(distances, list):
         distances = [distances]
+
+    # Make sure the first distance is 0
+    if distances[0] != 0:
+        distances = [0] + distances
+
+    # The maximum distance
     max_distance = max(distances)
 
     # return a copy of the sales
     salesWithFlags = sales.copy()
 
     # set flags to zero by default
-    for distance in distances:
+    for distance in distances[1:]:
         for tag in ["before", "after"]:
             salesWithFlags[f"spacetime_flag_{tag}_{distance}"] = 0
 
@@ -141,16 +152,19 @@ def add_spacetime_flags(homicides, sales, distances, windows, window_sales=True)
     for i, dt, indices, dists in groups:
 
         # do each distance flag
-        for distance in distances:
+        for j in range(0, len(distances) - 1):
             for I, D, tag in zip(indices, dists, ["before", "after"]):
 
                 # get the subset that satisfies this distance
-                valid = D < distance * FT_PER_MILE
+                valid = D >= distances[j] * FT_PER_MILE
+                valid &= D < distances[j + 1] * FT_PER_MILE
 
                 # the index values of before/after sales
                 subset = sales.index[I[valid]]
-                salesWithFlags.loc[subset, f"spacetime_flag_{tag}_{distance}"] = 1
+                salesWithFlags.loc[subset, f"spacetime_flag_{tag}_{distances[j+1]}"] = 1
 
+    # Remove any sales that occur close to the start/end of the time period
+    # being analyzed, such that they don't have a symmetric time frame
     if window_sales:
         min_time = salesWithFlags["sale_date"].min() + pd.Timedelta(
             f"{windows[0] * SECONDS_PER_DAY} seconds"
@@ -163,6 +177,31 @@ def add_spacetime_flags(homicides, sales, distances, windows, window_sales=True)
             salesWithFlags["sale_date"] <= max_time
         )
         salesWithFlags = salesWithFlags.loc[valid]
+
+    # Add the interaction flags
+    if add_interactions:
+
+        # Add coefficients for 0.5 * (after - before)
+        for dist in distances[1:]:
+            salesWithFlags[f"spacetime_flag_{dist}"] = 0.5 * (
+                salesWithFlags[f"spacetime_flag_after_{dist}"]
+                - salesWithFlags[f"spacetime_flag_before_{dist}"]
+            )
+
+        # Remove original columns
+        salesWithFlags = salesWithFlags.drop(
+            labels=[
+                f"spacetime_flag_{tag}_{dist}"
+                for dist in distances[1:]
+                for tag in ["after", "before"]
+            ],
+            axis=1,
+        )
+    else:
+        for dist in distances[1:]:
+            col = f"spacetime_flag_within_{dist}"
+            cols = [f"spacetime_flag_{tag}_{dist}" for tag in ["after", "before"]]
+            salesWithFlags[col] = salesWithFlags[cols].any(axis=1).astype(int)
 
     return salesWithFlags
 
